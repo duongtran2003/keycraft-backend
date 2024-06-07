6b1d4c1651c33b8cdb62b42daa4411a66a905dbd
duongtran2003 <dt005250@gmail.com>
2024-06-07 16:10:45 +0700

    reformat

--- src/controllers/chat.js
+++ src/controllers/chat.js
@@ -11,7 +11,11 @@ class ChatController {
 
     this.bard.initializeChat(process.env.BARD_API_KEY);
     const response = await this.bard.getBardResponse("Một câu hỏi nhỏ về bàn phím cơ (mechanical keyboard): " + message);
-    return res.status(200).json(response.text);
+    return res.status(200).json(
+      {
+        response: response.text
+      }
+    );
   }
 }
 
