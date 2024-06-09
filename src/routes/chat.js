d059f776525ff16e0e030b1d118243d6790c693d
duongtran2003 <dt005250@gmail.com>
2024-06-09 16:42:02 +0700

    bat lai jwt test ti

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { ChatController } from "../controllers/chat.js";
+import { jwtGuard } from "../middlewares/guards/jwtGuard.js";
 
 const router = new Router();
 const chatController = new ChatController();
 
-router.post('/getResponse', chatController.getResponse.bind(chatController));
+router.post('/getResponse', jwtGuard, chatController.getResponse.bind(chatController));
 
 export {
   router,
