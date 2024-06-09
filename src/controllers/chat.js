7407d1f4dd17b0625bbf26fb00a359d8b2001403
duongtran2003 <dt005250@gmail.com>
2024-06-09 15:38:01 +0700

    chinh sua linh tinh

--- src/controllers/chat.js
+++ src/controllers/chat.js
@@ -10,7 +10,7 @@ class ChatController {
     const message = req.body.message;
 
     this.bard.initializeChat(process.env.BARD_API_KEY);
-    const context = "Hãy tưởng tượng bạn là nhân viên tư vấn cho khách hàng về bàn phím cho cửa hàng bán phím, trả lời xúc tích ngắn gọn nhưng phải lễ phép lịch sử đầy đủ tế nhị. Sau đây là câu hỏi của tôi: ";
+    const context = "Hãy tưởng tượng bạn là nhân viên tư vấn cho khách hàng về bàn phím cho cửa hàng bán phím, trả lời xúc tích ngắn gọn nhưng phải lễ phép, lịch sự, tế nhị. Sau đây là câu hỏi của tôi: ";
     const response = await this.bard.getBardResponse(context + message);
     return res.status(200).json(
       {
